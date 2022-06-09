23cf585351cfece01efea6b0367acd39e40f20bc
Britney22 <102727336+Britney22@users.noreply.github.com>
2022-06-09 09:59:48 +0200

    code

--- MyWeatherAppSln/MyWeatherApp/MyWeatherApp/MainPage.xaml.cs
+++ MyWeatherAppSln/MyWeatherApp/MyWeatherApp/MainPage.xaml.cs
@@ -14,5 +14,24 @@ namespace MyWeatherApp
         {
             InitializeComponent();
         }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var data = await GetWeatherData();
+            BindingContext = data;
+        }
+        private async Task<OpenWeatherData> GetWeatherData()
+        {
+            var location = await Geolocation.GetLocationAsync();
+            var Latitude = location.Latitude;
+            var Longitude = location.Longitude;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("accept", "application/json");
+            var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=metric&appid=9f09b912cf5f374307477a500df40269");
+            var weatherData = JsonConvert.DeserializeObject<OpenWeatherData>(response);
+            return weatherData;
+        }
     }
 }
+    
+
